18b50bc711cb76cacfad2d76a0d230c21ed4ab99
saskiabt <sskbinder@gmail.com>
2022-08-31 16:32:39 -0400

    changed heading

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Heading() {
-  return <h1 className="title">Saskia&apos;s Weather App</h1>;
+  return <h1 className="title"> ~ Weather ~</h1>;
 }
 
 export default Heading;
